c6dac025f76a77155657dbab479ae2ddad8af934
samir <s-malik@live.co.uk>
2022-02-21 17:18:05 +0000

    Refactored tests to use let

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,25 +1,24 @@
 require './bank'
 
 describe Bank do
+
+    let (:bank) { Bank.new }
+
     it 'increases the balance by the amount deposited' do
-        bank = Bank.new
         bank.deposit(10,'10-10-2010')
         expect(bank.show_balance).to eq(10)
     end 
 
     it 'shows the balance starts at 0 on creation' do
-        bank = Bank.new
         expect(bank.show_balance).to eq(0)
     end 
 
     it 'decreases the balance by the amount withdrawn' do
-        bank = Bank.new
         bank.withdraw(10, '10-10-2010')
         expect(bank.show_balance).to eq(-10)
     end 
 
     it 'provides the correct balance for acceptance criteria' do
-        bank = Bank.new
         bank.deposit(1000, '10-01-2023')
         bank.deposit(2000, '13-01-2023')
         bank.withdraw(500, '14-01-2023')
