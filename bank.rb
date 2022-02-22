11a7da55a252e8b9533720aaa68360cecae72f78
samir <s-malik@live.co.uk>
2022-02-22 11:45:20 +0000

    Added final test and refactored method names

--- bank.rb
+++ bank.rb
@@ -1,35 +1,32 @@
 require 'terminal-table'
 
 class Bank
+  attr_accessor :balance, :transactions
 
-    attr_accessor :balance, :transactions
+  def initialize
+    @balance = 0
+    @transactions = []
+  end
 
-    def initialize
-        @balance = 0
-        @transactions = []
-    end 
+  def deposit(amount, date)
+    @balance += amount
+    @transactions << [date, amount, @balance]
+  end
 
-    def deposit(amount, date)
-        @balance += amount 
-        @transactions << [date, amount, @balance]
+  def withdraw(amount, date)
+    @balance -= amount
+    @transactions << [date, amount, @balance]
+  end
 
+  def show_balance
+    @balance
+  end
+
+  def print_statement
+    table = Terminal::Table.new do |t|
+      t.headings = %w[Date Amount Balance]
+      t.rows = @transactions.reverse
     end
-
-    def withdraw(amount, date)
-        @balance -= amount 
-        @transactions << [date, amount, @balance]
-    end 
-
-    def show_balance
-        @balance
-    end
-
-    def print_statement
-        table = Terminal::Table.new do |t|
-            t.headings = ['Date', 'Amount', 'Balance']
-            t.rows = @transactions.reverse
-        end
-        print table
-    end 
-
-end 
+    print table
+  end
+end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,29 +1,30 @@
 require './bank'
 
 describe Bank do
+  let(:bank) { Bank.new }
 
-    let (:bank) { Bank.new }
+  it 'increases the balance by the amount deposited' do
+    bank.deposit(10, '10-10-2010')
+    expect(bank.show_balance).to eq(10)
+  end
 
-    it 'increases the balance by the amount deposited' do
-        bank.deposit(10,'10-10-2010')
-        expect(bank.show_balance).to eq(10)
-    end 
+  it 'shows the balance starts at 0 on creation' do
+    expect(bank.show_balance).to eq(0)
+  end
 
-    it 'shows the balance starts at 0 on creation' do
-        expect(bank.show_balance).to eq(0)
-    end 
+  it 'decreases the balance by the amount withdrawn' do
+    bank.withdraw(10, '10-10-2010')
+    expect(bank.show_balance).to eq(-10)
+  end
 
-    it 'decreases the balance by the amount withdrawn' do
-        bank.withdraw(10, '10-10-2010')
-        expect(bank.show_balance).to eq(-10)
-    end 
+  it 'provides the correct balance for acceptance criteria' do
+    bank.deposit(1000, '10-01-2023')
+    bank.deposit(2000, '13-01-2023')
+    bank.withdraw(500, '14-01-2023')
+    expect(bank.show_balance).to eq(2500)
+  end
 
-    it 'provides the correct balance for acceptance criteria' do
-        bank.deposit(1000, '10-01-2023')
-        bank.deposit(2000, '13-01-2023')
-        bank.withdraw(500, '14-01-2023')
-        expect(bank.show_balance).to eq(2500)
-    end 
-
-
-end 
+  it 'tests whether the print_statement method responds' do
+    expect(bank).to respond_to(:print_statement)
+  end
+end
